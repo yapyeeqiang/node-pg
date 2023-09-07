01a40fc094fc318d4563c5ca13537eee7c6c0471
yapyeeqiang <yeeqiang@weblite.com.my>
2023-09-07 10:07:37 +0800

    add: cors

--- src/index.ts
+++ src/index.ts
@@ -1,6 +1,7 @@
 import "dotenv/config";
 import * as express from "express";
 import helmet from "helmet";
+import cors from "cors";
 import { AppDataSource } from "./data-source";
 import userRoutes from "./user/routes";
 import authRoutes from "./auth/routes";
@@ -18,6 +19,7 @@ const port = 3000;
 
 app.use(express.json());
 app.use(helmet());
+app.use(cors());
 
 app.use("/users", userRoutes);
 app.use("/", authRoutes);
